8a7752bcee6964be7bdffc15cf73d4285dd06652
MOREASHU <moreashwini65@gmail.com>
2018-11-25 14:39:54 +0530

    connect and disconnect event

--- server/server.js
+++ server/server.js
@@ -1,14 +1,27 @@
 // console.log(__dirname + '/public');
 
 const path =  require('path');
+const http = require('http');
 const express = require('express');
-var app = express();
+const socketIO = require('socket.io');
+
+
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath)); // middelware that serve the public folder
 
-app.listen(port,() => {
+io.on('connection', (socket) => {
+    console.log('new user connected');
+    socket.on('disconnect',() => {
+      console.log('user was disconnected');
+    });
+});
+
+server.listen(port,() => {
     console.log('server is up on 3000 port');
 })
 
