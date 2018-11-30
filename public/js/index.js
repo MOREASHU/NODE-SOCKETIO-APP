196f67e5055fd161091adfb8765d486170b0f841
MOREASHU <moreashwini65@gmail.com>
2018-11-30 22:41:28 +0530

    chat app with text and location send buttons

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'ashwini',
-        text: 'hey,hello how r u ?',
-    });
 });
 
 socket.on('disconnect', function() {
@@ -15,4 +10,52 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage',function(message) {
     console.log('newMessage',message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    jQuery('#messages').append(li);
+});
+
+socket.on('newLocationMessage',function (message) {
+   var li = jQuery('<li></li>');
+   var a = jQuery('<a target="_blank">my current location</a>');
+
+   li.text(`${message.from}: `);
+   a.attr('href',message.url);
+   li.append(a);
+   jQuery('#messages').append(li);
+});
+
+
+jQuery('#message-form').on('submit',function(e) {
+    e.preventDefault();
+    var messgaeTextbox = jQuery('[name=message]');
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: messgaeTextbox.val(),
+},function (){
+     messgaeTextbox.val('')   
+  });   
+});
+
+var locationButton = jQuery('#send-location');
+
+locationButton.on('click',function () {
+    if(!navigator.geolocation) {
+        return alert('Geolocation not supported by your browser')
+    }
+
+    locationButton.attr('disabled','disabled').text('sending location...');
+
+    navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send Location')
+        console.log(position);
+      socket.emit('createLocationMessage',{
+         latitude : position.coords.latitude,
+         longitude : position.coords.longitude
+      }); 
+    }, function () {
+        locationButton.removeAttr('disabled').text('Send Location')
+        alert('unable to fetch location');
+    });
 });
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const path =  require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const {generateMessage,generateLocationMessage} = require('./utils/message');
 
 
 const publicPath = path.join(__dirname,'../public');
@@ -17,14 +18,18 @@ app.use(express.static(publicPath)); // middelware that serve the public folder
 io.on('connection', (socket) => {
     console.log('new user connected');
 
-    socket.emit('newMessage', {
-        from : 'aarti',
-        text: 'hey, hi',
-        createdAt : 123
+    socket.emit('newMessage', generateMessage('Admin','Welcome to the chat app'));
+
+    socket.broadcast.emit('newMessage', generateMessage('Admin','New user joined'));
+
+    socket.on('createMessage', (message, callback) => {
+        console.log('createMessage',message);
+        io.emit('newMessage',generateMessage(message.from,message.text));
+        callback();
     });
 
-    socket.on('createMessage', (message) => {
-        console.log('createMessage',message);
+    socket.on('createLocationMessage',(coords) => {
+      io.emit('newLocationMessage', generateLocationMessage('Admin',coords.latitude,coords.longitude));
     })
 
     socket.on('disconnect',() => {
